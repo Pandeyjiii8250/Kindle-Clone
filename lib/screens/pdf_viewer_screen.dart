6e702deda16c03cb75352e02c82c9f6e8d5f631d
Pandeyjiii8250 <63416461+Pandeyjiii8250@users.noreply.github.com>
2025-06-23 08:54:14 +0530

    Add highlight option to PDF viewer context menu

--- lib/screens/pdf_viewer_screen.dart
+++ lib/screens/pdf_viewer_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kindle_clone_v2/models/book.dart';
 import 'package:pdfrx/pdfrx.dart';
 
+
 class PDFViewerScreen extends StatefulWidget {
   final Book bookDetail; // Can also be a URL
 
@@ -12,13 +13,54 @@ class PDFViewerScreen extends StatefulWidget {
 }
 
 class _PDFViewerScreenState extends State<PDFViewerScreen> {
+  String _currentSelection = '';
+
+  Widget _contextMenuBuilder(
+      BuildContext context, SelectableRegionState selectableRegionState) {
+    final List<ContextMenuButtonItem> items = [
+      ...selectableRegionState.contextMenuButtonItems,
+      ContextMenuButtonItem(
+        label: 'Highlight',
+        onPressed: () {
+          debugPrint('Highlighted text: $_currentSelection');
+          selectableRegionState.hideToolbar();
+        },
+      ),
+    ];
+
+    return AdaptiveTextSelectionToolbar.buttonItems(
+      anchors: selectableRegionState.contextMenuAnchors,
+      buttonItems: items,
+    );
+  }
+
+  void _onTextSelectionChange(List<PdfTextRanges> selections) {
+    if (selections.isEmpty) {
+      _currentSelection = '';
+      return;
+    }
+    final buffer = StringBuffer();
+    for (final range in selections) {
+      buffer.write(range.text);
+      debugPrint('Selected on page ${range.pageNumber}: ${range.text}');
+    }
+    _currentSelection = buffer.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.bookDetail.title)),
       body: PdfViewer.file(
         widget.bookDetail.filePath,
-        params: const PdfViewerParams(enableTextSelection: true),
+        params: PdfViewerParams(
+          enableTextSelection: true,
+          selectableRegionInjector: (context, child) => SelectionArea(
+            contextMenuBuilder: _contextMenuBuilder,
+            child: child,
+          ),
+          onTextSelectionChange: _onTextSelectionChange,
+        ),
       ),
     );
   }
