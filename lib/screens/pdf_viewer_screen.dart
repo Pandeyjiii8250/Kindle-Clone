a4581252e82cc8df59118ecb1b94f22876291ed3
rishabhpandey <rishabhpandey8250@gmail.com>
2025-06-24 11:24:52 +0530

    Highlights works but applies on every page :). Need to fix that.

--- lib/screens/pdf_viewer_screen.dart
+++ lib/screens/pdf_viewer_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:kindle_clone_v2/models/book.dart';
 import 'package:pdfrx/pdfrx.dart';
 
-
 class PDFViewerScreen extends StatefulWidget {
   final Book bookDetail; // Can also be a URL
 
@@ -14,15 +13,36 @@ class PDFViewerScreen extends StatefulWidget {
 
 class _PDFViewerScreenState extends State<PDFViewerScreen> {
   String _currentSelection = '';
+  List<Rect> _highlightRects = [];
+  List<PdfTextRanges> _selectedRanges = [];
+
+  void _addHighlight(List<Rect> rect) {
+    setState(() {
+      _highlightRects.addAll(rect);
+    });
+  }
 
   Widget _contextMenuBuilder(
-      BuildContext context, SelectableRegionState selectableRegionState) {
+    BuildContext context,
+    SelectableRegionState selectableRegionState,
+    PdfPage page,
+    Rect pageRect,
+  ) {
     final List<ContextMenuButtonItem> items = [
       ...selectableRegionState.contextMenuButtonItems,
       ContextMenuButtonItem(
         label: 'Highlight',
         onPressed: () {
           debugPrint('Highlighted text: $_currentSelection');
+          List<Rect> rect = [];
+          for (var range in _selectedRanges) {
+            final singleRect = range.bounds.toRect(
+              page: page,
+              scaledPageSize: pageRect.size,
+            );
+            rect.add(singleRect);
+          }
+          _addHighlight(rect);
           selectableRegionState.hideToolbar();
         },
       ),
@@ -35,16 +55,19 @@ class _PDFViewerScreenState extends State<PDFViewerScreen> {
   }
 
   void _onTextSelectionChange(List<PdfTextRanges> selections) {
+    setState(() {
+      _selectedRanges = selections;
+    });
+
     if (selections.isEmpty) {
       _currentSelection = '';
-      return;
+    } else {
+      final buffer = StringBuffer();
+      for (final range in selections) {
+        debugPrint('Selected on page ${range.pageNumber}: ${range.text}');
+      }
+      _currentSelection = buffer.toString();
     }
-    final buffer = StringBuffer();
-    for (final range in selections) {
-      buffer.write(range.text);
-      debugPrint('Selected on page ${range.pageNumber}: ${range.text}');
-    }
-    _currentSelection = buffer.toString();
   }
 
   @override
@@ -55,10 +78,28 @@ class _PDFViewerScreenState extends State<PDFViewerScreen> {
         widget.bookDetail.filePath,
         params: PdfViewerParams(
           enableTextSelection: true,
-          selectableRegionInjector: (context, child) => SelectionArea(
-            contextMenuBuilder: _contextMenuBuilder,
-            child: child,
-          ),
+          perPageSelectableRegionInjector: (context, child, page, pageRect) {
+            return Stack(
+              children: [
+                SelectionArea(
+                  contextMenuBuilder:
+                      (context, selectableRegionState) => _contextMenuBuilder(
+                        context,
+                        selectableRegionState,
+                        page,
+                        pageRect,
+                      ),
+                  child: child,
+                ),
+                ..._highlightRects.map(
+                  (rect) => Positioned.fromRect(
+                    rect: rect,
+                    child: Container(color: Colors.yellow.withOpacity(0.4)),
+                  ),
+                ),
+              ],
+            );
+          },
           onTextSelectionChange: _onTextSelectionChange,
         ),
       ),
